10ae801d892763d75fb0ccdeda485fe42df3cfe7
Matan Broner <matanbroner@gmail.com>
2022-02-06 19:41:44 -0600

    init repository

---
+++ README.md
@@ -0,0 +1,4 @@
+# Monkey Interpreter
+
+A basic interpreter written in Go. </br>
+Based on following the book, "Writing an Interpreter in Go"
---
+++ token/token.go
@@ -0,0 +1,32 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	IDENT = "IDENT" // x, y, add
+	INT   = "INT"
+
+	// Ops
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Syntax Chars
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
